72ca9825dc9faaffd6765c9ac5431571a6565bcf
Alice <chenyuxinalice@sina.com>
2016-11-18 23:55:59 -0500

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,17 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara'
+require 'capybara/dsl'
+#require 'capybara/rails'
 RSpec.configure do |config|
+  config.include Capybara::DSL, :type => :request
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  #config.include Capybara::DSL, :type => :request
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
